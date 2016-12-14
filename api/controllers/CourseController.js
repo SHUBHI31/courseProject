bcf4af18a9c52b4b359f526586e5f1ed2f48f246
SHUBHI31 <shubhiksj95@gmail.com>
2016-12-14 13:14:53 +0530

    "Initial Commit"

--- api/controllers/CourseController.js
+++ api/controllers/CourseController.js
@@ -10,28 +10,28 @@ module.exports = {
   {
     var result=req.param('searchstring');
 
-      Course.find({CourseName:result}).populate("univ_id").exec(function (err, usersName){
+      Course.findOne({CourseName:result}).populate("univ_id").exec(function (err, usersName){
         if (err) {
           return res.serverError(err);
         }
         if(typeof(usersName)!='undefined')
         return res.json(usersName);
         else {
-          University.find({UnivName: result}).populate("allcourses").exec(function (err, usersName) {
+          University.findOne({UnivName: result}).populate("allcourses").exec(function (err, usersName) {
             if (err) {
               return res.serverError(err);
             }
 
+
             return res.json(usersName);
           });
+
+
         }
-      });
+      } );
+
 
   },
-  'new' : function(req,res)
-  {
-    res.view();
-  }
 
 };
 
